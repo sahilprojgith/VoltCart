99e178d8c5b1b9b94f4bc74766f627a68aeae21f
Sahil sanjay more <108298194+sahilprojgith@users.noreply.github.com>
2025-08-04 23:17:32 +0530

    Update HelloController.java

--- SpringEcom/SpringEcom/src/main/java/com/telusko/SpringEcom/controller/HelloController.java
+++ SpringEcom/SpringEcom/src/main/java/com/telusko/SpringEcom/controller/HelloController.java
@@ -1,4 +1,4 @@
-package com.telusko.SpringEcom.controller;
+package com.sahil.SpringEcom.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,3 +11,4 @@ public class HelloController {
         return "Welcome to this madness!!";
     }
 }
+
