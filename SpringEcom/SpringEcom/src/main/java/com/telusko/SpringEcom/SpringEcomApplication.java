039db25b0693f3b58cb10085bcaef59d77303e57
Sahil sanjay more <108298194+sahilprojgith@users.noreply.github.com>
2025-08-04 23:18:49 +0530

    Update SpringEcomApplication.java

--- SpringEcom/SpringEcom/src/main/java/com/telusko/SpringEcom/SpringEcomApplication.java
+++ SpringEcom/SpringEcom/src/main/java/com/telusko/SpringEcom/SpringEcomApplication.java
@@ -1,4 +1,4 @@
-package com.telusko.SpringEcom;
+package com.sahil.SpringEcom;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
